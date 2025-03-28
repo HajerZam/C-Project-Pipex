d26e4134454c6a0a3f6bd7830d8fc7324132fdb6
Meiphim_ <hajer.zmazi2004@gmail.com>
2025-03-28 14:29:33 +0100

    Update pipex_helper.c

--- src/pipex_helper.c
+++ src/pipex_helper.c
@@ -25,24 +25,47 @@ void	create_child(char *cmd, char **envp, int *fd, int infile)
 
 void	create_parent(char *cmd, char **envp, int *fd, int outfile)
 {
-	wait(NULL);
-	parent_process(cmd, envp, fd, outfile);
+	pid_t	pid;
+
+	pid = fork();
+	if (pid < 0)
+		error("fork");
+	if (pid == 0)
+		parent_process(cmd, envp, fd, outfile);
 }
 
 void	child_process(char *cmd, char **envp, int *fd, int infile)
 {
 	close(fd[0]);
+	
+	if (infile < 0)
+	{
+		close(fd[1]);
+		exit(EXIT_FAILURE);
+	}
+
 	dup2(fd[1], STDOUT_FILENO);
 	dup2(infile, STDIN_FILENO);
 	close(fd[1]);
+	close(infile);
+	
 	execute(cmd, envp);
 }
 
 void	parent_process(char *cmd, char **envp, int *fd, int outfile)
 {
 	close(fd[1]);
+	
+	if (outfile < 0)
+	{
+		close(fd[0]);
+		exit(EXIT_FAILURE);
+	}
+
 	dup2(fd[0], STDIN_FILENO);
 	dup2(outfile, STDOUT_FILENO);
 	close(fd[0]);
+	close(outfile);
+	
 	execute(cmd, envp);
 }
